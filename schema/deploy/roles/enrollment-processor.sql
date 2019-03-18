85c87cb67f7b9fdc9566f417a78116e369e8981e
Thomas Sibley <tsibley@fredhutch.org>
2019-03-18 09:56:43 -0700

    Add an "enrollment_processor" role for the automated enrollment ETL process
    Following the principle of least privilege, this limits what a haywire
    process can do.  The role's privileges will need to be updated as the
    warehouse and ETL process changes.

---
+++ schema/deploy/roles/enrollment-processor.sql
@@ -0,0 +1,29 @@
+-- Deploy seattleflu/schema:roles/enrollment-processor to pg
+-- requires: receiving/enrollment/processing-log
+-- requires: warehouse/schema
+
+begin;
+
+create role enrollment_processor;
+
+grant connect on database :"DBNAME" to enrollment_processor;
+
+grant usage
+   on schema receiving, warehouse
+   to enrollment_processor;
+
+grant select
+   on receiving.enrollment
+   to enrollment_processor;
+
+grant update (processing_log)
+   on receiving.enrollment
+   to enrollment_processor;
+
+grant select, insert, update
+   on warehouse.site,
+      warehouse.individual,
+      warehouse.encounter
+   to enrollment_processor;
+
+commit;
---
+++ schema/revert/roles/enrollment-processor.sql
@@ -0,0 +1,27 @@
+-- Revert seattleflu/schema:roles/enrollment-processor from pg
+
+begin;
+
+revoke select, insert, update
+    on warehouse.site,
+       warehouse.individual,
+       warehouse.encounter
+  from enrollment_processor;
+
+revoke update (processing_log)
+    on receiving.enrollment
+  from enrollment_processor;
+
+revoke select
+    on receiving.enrollment
+  from enrollment_processor;
+
+revoke usage
+    on schema receiving, warehouse
+  from enrollment_processor;
+
+revoke connect on database :"DBNAME" from enrollment_processor;
+
+drop role enrollment_processor;
+
+commit;
