5f0b0263f69714883a182214a63e78b566729d0a
Thomas Sibley <tsibley@fredhutch.org>
2019-04-04 15:05:08 -0700

    Unify logging across modules in our Python code
    This moves two similar logging setups from seattleflu.api and
    seattleflu.db.cli into seattleflu where it is shared.
    
    Logging should be something that Just Works and works consistently
    across a shared codebase.

--- lib/seattleflu/api/__init__.py
+++ lib/seattleflu/api/__init__.py
@@ -2,26 +2,12 @@
 Seattle Flu Study informatics API
 """
 import logging
-import os
 from flask import Flask
 from . import config
 from .router import blueprints
 
 
-# Setup logging
-handler = logging.StreamHandler()
-
-handler.setFormatter(
-    logging.Formatter(
-        '[%(asctime)s] %(name)s %(levelname)s: %(message)s'))
-
 LOG = logging.getLogger(__name__)
-LOG.addHandler(handler)
-
-LOG_LEVEL = os.environ.get("LOG_LEVEL")
-
-if LOG_LEVEL:
-    LOG.setLevel(LOG_LEVEL.upper())
 
 
 def create_app():
@@ -30,9 +16,6 @@ def create_app():
 
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
-
-    if app.debug:
-        LOG.setLevel("DEBUG")
 
     LOG.debug(f"app root is {app.root_path}")
     LOG.debug(f"app static directory is {app.static_folder}")
