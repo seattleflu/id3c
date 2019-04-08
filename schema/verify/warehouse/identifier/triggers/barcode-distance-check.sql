8c35afe39e2c28d08c40c483914861f5d3d59f1f
Thomas Sibley <tsibley@fredhutch.org>
2019-04-08 11:31:03 -0700

    Test UPDATE handling of identifier barcode distance check
    I overlooked these previously, but we definitely want to make sure it
    works as expected.

--- schema/verify/warehouse/identifier/triggers/barcode-distance-check.sql
+++ schema/verify/warehouse/identifier/triggers/barcode-distance-check.sql
@@ -23,6 +23,24 @@ begin
 
     insert into warehouse.identifier (uuid, identifier_set_id)
         values ('655add66-4898-4105-be09-0d1b00000123'::uuid, testing_set);
+
+    -- Too close to existing other barcode
+    begin
+        update warehouse.identifier
+           set uuid = '655add66-4898-4105-be09-0d1b00000012'::uuid,
+               barcode = '00000012'
+         where barcode = '00000123';
+        assert false, 'too similar barcode allowed';
+    exception
+        when exclusion_violation then
+            null; -- expected
+    end;
+
+    -- Close to itself (ok), but far enough from existing other barcode
+    update warehouse.identifier
+       set uuid = '655add66-4898-4105-be09-0d1b00000124'::uuid,
+           barcode = '00000124'
+     where barcode = '00000123';
 end
 $$;
 
