8ef82f50f9155473494d0645053a2de9f4d629f9
Thomas Sibley <tsibley@fredhutch.org>
2019-05-03 12:37:01 -0700

    Add presence-absence-processor role
    Co-authored-by: Kairsten Fay <kairsten.fay@gmail.com>
    Co-authored-by: Thomas Sibley <tsibley@fredhutch.org>

---
+++ schema/revert/roles/presence-absence-processor.sql
@@ -0,0 +1,32 @@
+-- Revert seattleflu/schema:roles/presence-absence-processor from pg
+
+begin;
+
+revoke select
+    on warehouse.identifier,
+       warehouse.identifier_set
+  from "presence-absence-processor";
+
+revoke select, insert, update
+    on warehouse.sample,
+       warehouse.target,
+       warehouse.presence_absence
+  from "presence-absence-processor";
+
+revoke update (processing_log)
+    on receiving.presence_absence
+  from "presence-absence-processor";
+
+revoke select
+    on receiving.presence_absence
+  from "presence-absence-processor";
+
+revoke usage
+    on schema receiving, warehouse
+  from "presence-absence-processor";
+
+revoke connect on database :"DBNAME" from "presence-absence-processor";
+
+drop role "presence-absence-processor";
+
+commit;
