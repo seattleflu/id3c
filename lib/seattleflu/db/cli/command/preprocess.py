f326d391eb85622b7d6ac234a10fbeb8288d2485
Jover <joverlee521@gmail.com>
2019-05-13 14:14:35 -0700

    Add preprocess cli command
    Co-authored-by: Kairsten Fay <kairsten.fay@gmail.com>
    
    Given clinical data Excel doc that contains
    PII and unnecessary information, pull out
    specific columns of interest and hash PII.
    
    Currently joins with manifests of retrospective
    specimen to find corresponding SFS barcodes.
    
    Still missing census tract information and
    vaccine information.

---
+++ lib/seattleflu/db/cli/command/preprocess.py
@@ -0,0 +1,153 @@
+"""
+Pre-process clinical data.
+
+Clinical data will contain PII(personally identifiable information) and 
+unnecessary information that do not need to be stored. This process will only
+pull out specific columns of interest that will then be stored in the
+receiving schema of ID3C.
+"""
+import click
+import logging
+import os
+import pandas as pd
+import hashlib
+import seattleflu.db as db
+from seattleflu import labelmaker
+from seattleflu.db.session import DatabaseSession
+from seattleflu.db.cli.__main__ import cli
+
+
+LOG = logging.getLogger(__name__)
+
+
+@cli.group("preprocess", help = __doc__)
+def preprocess():
+    pass
+
+# Clinical subcommand
+@preprocess.command("uw_clinical")
+@click.argument("filename", metavar = "<Clinical Data filename>")
+@click.argument("uw_nwh_file", metavar="<UW/NWH filename>")
+@click.argument("hmc_sch_file", metavar="<HMC/SCH filename>")
+
+
+def uw_clinical(filename, uw_nwh_file, hmc_sch_file):
+    """
+    Process and insert clinical data from UW.
+
+    Given a *filename* to an Excel document, selects specific columns of 
+    interest and inserts the queried data into the receiving.clinical table as 
+    a JSON document.
+
+    Specifically looks for a sheet named 'pts' and assumes that each row
+    represents one encounter.
+
+    Uses *uw_nwh_file* and *hmc_sch_file* to join clinical data to fill in
+    SFS barcodes. 
+
+    Hint: ~/Documents/Inpatient\ Clinical\ Data\ Pulls/Datasets/4.9.19.xlsx
+        ~/Documents/Inpatient Clinical Data Pulls/Datasets/4.9.19.xlsx
+    """
+    df = pd.read_excel(filename, sheet_name='pts')
+    uw_df = pd.read_excel(uw_nwh_file,sheet_name='UWMC',keep_default_na=False,
+                          usecols=['Barcode ID', 'MRN', 'Accession'])
+    nwh_df = pd.read_excel(uw_nwh_file,sheet_name='NWH',keep_default_na=False,
+                           usecols=['Barcode ID', 'MRN', 'Accession'])
+    hmc_df = pd.read_excel(hmc_sch_file,sheet_name='HMC',keep_default_na=False,
+                           usecols=['Barcode ID', 'MRN', 'Accession'])
+    
+    #Generate a unique identifier for each encounter and drop duplicates
+    df['identifier'] = (df['labMRN'] + df['LabAccNum']).str.lower()
+    df = df.drop_duplicates(subset="identifier")
+
+    #Join with vaccine sheet to get flu shot response
+    try:  # Join on vaccines? TODO 
+        df = join_vaccines_sheet(df, filename)
+    except:
+        pass
+    
+    #Add barcode using manifests
+    master_ref = hmc_df.append([uw_df, nwh_df], ignore_index=True)
+    # Convert all to lower case for matching purposes
+    master_ref['MRN'] = master_ref['MRN'].str.lower()
+    master_ref['Accession'] = master_ref['Accession'].str.lower()
+    master_ref['Barcode ID'] = master_ref['Barcode ID'].str.lower()
+    df['MRN'] = df['MRN'].str.lower()
+    df['Accession'] = df['Accession'].str.lower()
+    #Join on MRN and Accession
+    df = df.merge(master_ref, how='left', on=['MRN','Accession'])
+    
+    #Drop unnecessary columns
+    columns_to_keep = ["PersonID", "Age", "LabDtTm", "identifier", "Sex", 
+                       "Race", "Fac", "RESULT_VAL", "EthnicGroup",
+                       "FinClass", "Barcode ID", "ZipCode"]# To be replaced w/ census tract  
+    columns = df.columns.tolist()
+    columns_to_drop = [col for col in columns if col not in columns_to_keep]
+    df = df.drop(columns=columns_to_drop, axis=1)
+
+    #Standardize column names
+    df = df.rename(columns={'PersonID': 'individual', 
+                            'Age': 'age', 
+                            'LabDtTm': 'encountered',
+                            'EthnicGroup': 'HispanicLatino',
+                            'Sex': 'AssignedSex',
+                            'Fac': 'site', 
+                            'RESULT_VAL': 'FluShot',
+                            'FinClass': 'MedicalInsurance',
+                            'Barcode ID': 'barcode'})
+
+    # Convert datatype in 'age' column because
+    # Pandas does not support NaNs with normal type 'int'
+    df['age'] = df['age'].astype(pd.Int64Dtype())
+
+    #Hash PersonID and encounter identifier(MRN+Accession)
+    df['individual'] = df['individual'].apply(generate_hash)
+    df['identifier'] = df['identifier'].apply(generate_hash)
+
+    session = DatabaseSession()
+    with session, session.cursor() as cursor:
+        df.apply(lambda x: insert_clinical(x, cursor), axis=1)
+
+def join_vaccines_sheet(df: pd.DataFrame, filename):
+    """
+    Given a pandas DataFrame *df*, joins it on a one-to-one basis on the column 
+    'EID' with a second pandas DataFrame created from the given *filename* Excel 
+    document on the sheet named 'vaccine'.
+    """
+    vac = pd.read_excel(filename, sheet_name='vaccine')
+    
+    # While there are duplicates, arbitrarily keep only the first EID TODO 
+    vac = vac.drop_duplicates(subset='EID')
+
+    assert len(vac['EID'].unique()) == len(vac['EID']) 
+    df = df.merge(vac, how='left', on='EID', validate='one_to_one')
+
+    return df
+
+def generate_hash(identifier: str):
+    """
+    Generate hash for *identifier* that is linked to identifiable records. 
+    Must provide a "PARTICIPANT_DEIDENTIFIER_SECRET" as an OS environment
+    variable. 
+    """
+    secret = os.environ["PARTICIPANT_DEIDENTIFIER_SECRET"]
+    new_hash = hashlib.sha256()
+    new_hash.update(identifier.encode("utf-8"))
+    new_hash.update(secret.encode("utf-8"))
+    return new_hash.hexdigest()
+
+
+def insert_clinical(df: pd.DataFrame, cursor):
+    """
+    Given a pandas DataFrame, inserts it as a JSON document into the 
+    receiving.clinical table 
+    """
+    
+    LOG.debug(f"Inserting clinical data for «{df['identifier']}» ")
+
+    document = df.to_json(date_format='iso')
+
+    cursor.execute(
+        "insert into receiving.clinical (document) values (%s)",
+        (document,)
+    )
