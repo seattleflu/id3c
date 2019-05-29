ba4a58cfa5e57891848870d859f7c1ee39da8b68
Kairsten Fay <kairsten.fay@gmail.com>
2019-05-28 18:00:44 -0700

    Create identifier-minter role
    Creates an identifier-minter role in the database via sqitch.

---
+++ schema/verify/roles/identifier-minter/grants.sql
@@ -0,0 +1,14 @@
+-- Verify seattleflu/schema:roles/identifier-minter/grants on pg
+
+begin;
+
+select 1/pg_catalog.has_database_privilege('identifier-minter', :'DBNAME', 'connect')::int;
+select 1/pg_catalog.has_schema_privilege('identifier-minter', 'warehouse', 'usage')::int;
+select 1/pg_catalog.has_table_privilege('identifier-minter', 'warehouse.identifier', 'select,insert')::int;
+select 1/pg_catalog.has_table_privilege('identifier-minter', 'warehouse.identifier_set', 'select')::int;
+
+select 1/(not pg_catalog.has_schema_privilege('identifier-minter', 'receiving', 'usage'))::int;
+select 1/(not pg_catalog.has_table_privilege('identifier-minter', 'warehouse.identifier', 'update,delete'))::int;
+select 1/(not pg_catalog.has_table_privilege('identifier-minter', 'warehouse.identifier_set', 'insert,update,delete'))::int;
+
+rollback;
