a73c871caddbdc94addcb15747097f32cfebbe93
Thomas Sibley <tsibley@fredhutch.org>
2019-10-18 09:47:07 -0700

    Make roles/fhir-uploader/grants actually reworkable without adjustment
    The leading "revoke everything" stanza necessary for easy reworks was
    missing from my original pattern for schema/deploy/roles/…/grants.sql
    files, and its omission has been copied into several other roles created
    since then.  We've added it to some of those when reworking them, but
    going forward, it's nice not to introduce new cases of the omission
    which we'll only have to correct later.

--- schema/deploy/roles/fhir-uploader/grants.sql
+++ schema/deploy/roles/fhir-uploader/grants.sql
@@ -1,12 +1,20 @@
 -- Deploy seattleflu/schema:roles/fhir-uploader/grants to pg
 -- requires: roles/fhir-uploader/create
 -- requires: receiving/fhir
+-- requires: warehouse/schema
+-- requires: shipping/schema
 
 begin;
 
 -- This change is designed to be sqitch rework-able to make it easier to update
 -- the grants for this role.
 
+-- Revoke everything…
+revoke all on database :"DBNAME" from "fhir-uploader";
+revoke all on schema receiving, warehouse, shipping from "fhir-uploader";
+revoke all on all tables in schema receiving, warehouse, shipping from "fhir-uploader";
+
+-- …then re-grant
 grant connect on database :"DBNAME" to "fhir-uploader";
 
 grant usage
