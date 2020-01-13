a6f30b5901264062f0d8ecd38ff5a7d902e051e4
Jover <joverlee521@gmail.com>
2020-01-13 15:49:05 -0800

    shipping views: Remove `drop view...` statements
    Remove the `drop view...` statements used for removing SFS specific
    from ID3C core. We do not want to include these statements in future
    shipping view reworks so that the SFS views are not dropped from
    the database.

---
+++ schema/verify/shipping/views@2020-01-14c.sql
@@ -0,0 +1,10 @@
+-- Verify seattleflu/schema:shipping/views on pg
+
+begin;
+
+select 1/(count(*) = 1)::int
+  from information_schema.views
+ where array[table_schema, table_name]::text[]
+     = pg_catalog.parse_ident('shipping.presence_absence_result_v1');
+
+rollback;
