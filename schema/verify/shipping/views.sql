6f00237c57432c020ae7c977745d9d4c40b1e98c
Jover <joverlee521@gmail.com>
2020-01-13 15:42:07 -0800

    shipping views: remove SFS specific views
    These SFS specific shipping views will be ported over to
    ID3C-customizations.
    
    This schema change should be deployed with the sqitch's `--log-only`
    option since we don't actually want to drop these views from the
    database.

--- schema/verify/shipping/views.sql
+++ schema/verify/shipping/views.sql
@@ -5,31 +5,6 @@ begin;
 select 1/(count(*) = 1)::int
   from information_schema.views
  where array[table_schema, table_name]::text[]
-     = pg_catalog.parse_ident('shipping.incidence_model_observation_v1');
-
-select 1/(count(*) = 1)::int
-  from information_schema.views
- where array[table_schema, table_name]::text[]
      = pg_catalog.parse_ident('shipping.presence_absence_result_v1');
-
-select 1/(count(*) = 1)::int
-  from information_schema.views
- where array[table_schema, table_name]::text[]
-     = pg_catalog.parse_ident('shipping.incidence_model_observation_v2');
-
-select 1/(count(*) = 1)::int
-  from information_schema.views
- where array[table_schema, table_name]::text[]
-     = pg_catalog.parse_ident('shipping.observation_with_presence_absence_result_v1');
-
-select 1/(count(*) = 1)::int
-  from information_schema.views
- where array[table_schema, table_name]::text[]
-     = pg_catalog.parse_ident('shipping.incidence_model_observation_v3');
-
-select 1/(count(*) = 1)::int
-  from information_schema.views
- where array[table_schema, table_name]::text[]
-     = pg_catalog.parse_ident('shipping.observation_with_presence_absence_result_v2');
 
 rollback;
