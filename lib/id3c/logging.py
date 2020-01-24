11b6323b528d380ba093a5756b0c718697bea9ad
Thomas Sibley <tsibley@fredhutch.org>
2020-01-24 13:13:44 -0800

    logging: Support custom logging config files
    This allows our different environments (production, testing, dev, etc)
    to fine tune logging if needed.
    
    The stock config files should remain fairly generic.

--- lib/id3c/logging.py
+++ lib/id3c/logging.py
@@ -11,6 +11,7 @@ import yaml
 from pkg_resources import resource_stream
 
 
+LOG_CONFIG = os.environ.get("LOG_CONFIG")
 LOG_LEVEL = os.environ.get("LOG_LEVEL")
 IS_DEBUG = LOG_LEVEL and LOG_LEVEL.upper() == "DEBUG"
 
@@ -25,6 +26,14 @@ def configure():
     Python library warnings are captured and logged at the ``WARNING`` level.
     Uncaught exceptions are logged at the ``CRITICAL`` level before they cause
     the process to exit.
+
+    Finally, if a custom configuration file is specified with ``LOG_CONFIG``,
+    it is loaded as YAML and applied with :py:meth:`logging.config.dictConfig`.
+    The file itself has control over whether it replaces or overlays the
+    existing stock config using the ``incremental`` and
+    ``disable_existing_loggers``.  Two custom YAML tags, ``!LOG_LEVEL`` and
+    ``!coalesce``, are supported to aid with configs; see
+    :py:class:`LogConfigLoader` for more details.
     """
     stock_config = load_stock_config("debug" if IS_DEBUG else "default")
     logging.config.dictConfig(stock_config)
@@ -35,6 +44,10 @@ def configure():
     # Log any uncaught exceptions which are about to cause process exit.
     sys.excepthook = (lambda *args:
         logging.getLogger().critical("Uncaught exception:", exc_info = args)) # type: ignore
+
+    if LOG_CONFIG:
+        with open(LOG_CONFIG, "rb") as file:
+            logging.config.dictConfig(load_config(file))
 
 
 def load_stock_config(name = "default"):
