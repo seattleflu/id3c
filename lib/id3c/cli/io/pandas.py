e0c97779ecd6a86c43aad4d9cf3ca4cc35bd228a
Thomas Sibley <tsibley@fredhutch.org>
2020-02-12 17:20:44 -0800

    Truly, really read values in Excel files into a DataFrame of strings
    If a manifest, for example, comes in with a column of mixed types, we
    don't want to error out.  We want to just treat everything as a string
    and deal with problems stemming from that later instead of blocking
    import.  For example, if ids lose their leading zeros because they were
    accidentally stored in Excel as numbers, not text, we want to find that
    out when the id fails to match during ETL ingest instead of blowing up
    during parsing of a file we don't control.

--- lib/id3c/cli/io/pandas.py
+++ lib/id3c/cli/io/pandas.py
@@ -5,6 +5,7 @@ import click
 import logging
 import pandas as pd
 from textwrap import dedent
+from typing import List
 
 
 LOG = logging.getLogger(__name__)
@@ -41,7 +42,7 @@ def load_file_as_dataframe(filename: str) -> pd.DataFrame:
         separator = '\t' if filename.endswith('.tsv') else ','
         df = pd.read_csv(filename, sep=separator, dtype="string", na_filter=False)
     else:
-        df = pd.read_excel(filename, dtype="string", na_filter=False)
+        df = read_excel(filename, na_filter=False)
 
     return df
 
@@ -58,6 +59,51 @@ def load_input_from_file_or_stdin(filename: click.File) -> pd.DataFrame:
         input_df = load_file_as_dataframe(filename.name)
 
     return input_df
+
+
+def read_excel(io, sheet_name = None, na_filter: bool = True):
+    """
+    Reads an Excel file while ensuring all values are treated as strings.
+
+    This bends over backwards to make ``pandas.read_excel(..., dtype =
+    "string")`` behave like you might hope (all cells are read as or cast to
+    string values), instead of how it actually does (cells are read as native
+    types and an exception is thrown if there are non-string values).
+
+    Due to the gymnastics performed, most optional arguments to
+    :py:func:`pandas.read_excel` are not initially supported.  However, support
+    for these missing options could be added in the future with additional
+    logic.
+    """
+    # Read header row and zero data rows for the sheet(s)
+    sheet_headers = pd.read_excel(io, sheet_name = sheet_name, nrows = 0)
+
+    # Read each sheet, specifying str() as a converter function for each
+    # column.  As documented by Pandas, converters takes precedence over dtype
+    # and acts earlier.  Once we have an object-dtype DataFrame (containing
+    # strings), we can convert that to a string-dtype DataFrame without risk of
+    # errors being thrown if an Excel cell is typed as a number.
+    def read_sheet(sheet: str, columns: List[str]) -> pd.DataFrame:
+        converters = {
+            column: str
+                for column in columns }
+
+        return (
+            pd
+            .read_excel(
+                io,
+                sheet_name = sheet,
+                na_filter = na_filter,
+                converters = converters)
+            .astype("string"))
+
+    # Multiple sheets at a time are supported.
+    if isinstance(sheet_headers, dict):
+        return {
+            sheet: read_sheet(sheet, header.columns)
+                for sheet, header in sheet_headers.items() }
+    else:
+        return read_sheet(sheet_name, sheet_headers.columns)
 
 
 class UnsupportedFileExtensionError(ValueError):
