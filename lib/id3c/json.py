0c5e1e3dea285cebd17428c3cc83cbc292b421dd
Thomas Sibley <tsibley@fredhutch.org>
2021-04-02 14:11:41 -0700

    json: Add load_json() function which contextualizes decoding errors
    Snips the source document around the error position for context and
    marks the problematic character.
    
    One slight concern here is the chance for sensitive information to slip
    in the logs via JSON content (e.g. received from the REDCap API).  I
    think this is likely to be ok, as the expectation is not that malformed
    JSON is sent, but that non-JSON text is are sent.  In the case of
    malformed JSON, the document will be snipped to 21 chars, reducing both
    log message length and chance of sensitive information leaking.

--- lib/id3c/json.py
+++ lib/id3c/json.py
@@ -4,6 +4,7 @@ Standardized JSON conventions.
 import json
 from datetime import datetime
 from typing import Iterable
+from .utils import contextualize_char
 
 
 def as_json(value):
@@ -11,6 +12,19 @@ def as_json(value):
     Converts *value* to a JSON string using our custom :class:`JsonEncoder`.
     """
     return json.dumps(value, allow_nan = False, cls = JsonEncoder)
+
+
+def load_json(value):
+    """
+    Converts *value* from a JSON string with better error messages.
+
+    Raises an :exc:`id3c.json.JSONDecodeError` which provides improved error
+    messaging, compared to :exc:`json.JSONDecodeError`, when stringified.
+    """
+    try:
+        return json.loads(value)
+    except json.JSONDecodeError as e:
+        raise JSONDecodeError(e) from e
 
 
 def dump_ndjson(iterable: Iterable) -> None:
@@ -25,7 +39,7 @@ def load_ndjson(file: Iterable[str]) -> Iterable:
     """
     Load newline-delimited JSON records from *file*.
     """
-    yield from (json.loads(line) for line in file)
+    yield from (load_json(line) for line in file)
 
 
 class JsonEncoder(json.JSONEncoder):
@@ -46,3 +60,45 @@ class JsonEncoder(json.JSONEncoder):
         else:
             # Let the base class raise the TypeError
             return super().default(value)
+
+
+class JSONDecodeError(json.JSONDecodeError):
+    """
+    Subclass of :class:`json.JSONDecodeError` which contextualizes the
+    stringified error message by including a snippet of the JSON source input.
+
+    Typically you won't need to ever reference this class directly.  It will be
+    raised by :func:`load_json` and be caught by except blocks which catch the
+    standard :class:`json.JSONDecodeError`.
+
+    >>> load_json('{foo: "bar"}')
+    Traceback (most recent call last):
+        ...
+    id3c.json.JSONDecodeError: Expecting property name enclosed in double quotes: line 1 column 2 (char 1): {▸▸▸f◂◂◂oo: "bar"}
+
+    >>> load_json('not json')
+    Traceback (most recent call last):
+        ...
+    id3c.json.JSONDecodeError: Expecting value: line 1 column 1 (char 0): not json
+
+    >>> load_json('')
+    Traceback (most recent call last):
+        ...
+    id3c.json.JSONDecodeError: Expecting value: line 1 column 1 (char 0): (empty source document)
+    """
+    def __init__(self, exc: json.JSONDecodeError):
+        super().__init__(exc.msg, exc.doc, exc.pos)
+
+    def __str__(self):
+        error = super().__str__()
+
+        if self.doc:
+            if self.pos == 0 and self.msg == "Expecting value":
+                # Most likely not a JSON document at all, so show the whole thing.
+                context = self.doc
+            else:
+                context = contextualize_char(self.doc, self.pos)
+        else:
+            context = "(empty source document)"
+
+        return f"{error}: {context}"
