bd5da321a0cc74fddc7cb5fce59516baa4803992
Todd Seidelmann <seidelma@uw.edu>
2022-03-04 00:36:13 +0000

    Add openpyxl as a pandas dependency for parsing Excel files

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         "jsonschema",
         "more-itertools",
         "oauth2client >2.0.0,<4.0.0",
+        "openpyxl",
         "pandas",
         "psycopg2 >=2.8,<3",
         "pyyaml",
